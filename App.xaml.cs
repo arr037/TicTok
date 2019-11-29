8c35ea963925231b57ac7cfc16c9babd5dff91bc
f22 <f22@microsoft365now.com>
2019-11-29 22:56:26 +0600

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Ioc;
+using TicTok.Views;
+using System.Windows;
+using Prism.Modularity;
+using TicToke.Module.Page;
+
+namespace TicTok
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<PageModule>();
+        }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Prism.Mvvm;
+
+namespace TicTok.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title => "Prism Application";
+        public MainWindowViewModel()
+        {
+          
+        }
+    }
+}
